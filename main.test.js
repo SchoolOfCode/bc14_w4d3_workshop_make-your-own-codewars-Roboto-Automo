ed58e58563818c05ca85814949fd6c0fe6d3e725
Automoroboto <roberto@studiodespirito.co.uk>
2023-04-07 11:31:23 +0100

    updated description and added one test

--- main.test.js
+++ main.test.js
@@ -1,1 +1,14 @@
 //👉 Write your tests below here:
+
+import {expect, jest, test} from '@jest/globals';
+
+import { calculateUKIncomeAfterTax } from './main';
+
+
+test("Test for gross income £60000", ()=> {
+
+const actual = calculateUKIncomeAfterTax(60000)
+const expected = "£48568"
+expect(actual).toBe(expected)
+
+} )
