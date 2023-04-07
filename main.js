4f7a7f35514c4551a37cebced5eeab45d3afc5a2
Automoroboto <roberto@studiodespirito.co.uk>
2023-04-07 10:51:32 +0100

    write initial problem

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
-👉 Write your kata here!
+April has sprung and it's time to do your taxes UK dwellers!  We can't be bothered to do all the calculations each year so it's time to write a function that does it all for us.
+
+Using the tax bandings below, write a function that takes in a gross total taxable income as an argument and returns what the net income is after deducting tax owed.
+
+Personal Allowance	Up to £12,570	you pay 0% tax 
+Basic rate	£12,571 to £50,270	    you pay 20% tax
+Higher rate	£50,271 to £125,140	    you pay 40% tax
+Additional rate	over £125,140	    you pay 45% tax
+
+As an example, if the gross total was £11000, the function should return £11000 since there is no tax to be paid on this amount.
+
+If the gross total was £60000, the function should return £48568, because the tax to pay between £0 and £12570 is £0, the tax to pay between £12571 and £50,270 is £7540 and the tax to pay between £50,271 and £60000 is £3892.
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+function calculateIncomeAfterTax(taxableIncome){
+// write your code here 
+
+
+}
